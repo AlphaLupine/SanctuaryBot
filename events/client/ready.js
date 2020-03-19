4735183388fa741bf086035962da60e565b9315d
AlphaLupine <matthewduff0912@gmail.com>
2020-03-19 22:04:12 +0000

    [EDIT] Added a log to indicate when when the bot becomes ready

--- events/client/ready.js
+++ events/client/ready.js
@@ -1,5 +1,7 @@
 
 module.exports = async client => {
+
+    console.log(`${client.user.tag} is now online!`)
     
     let activities = [ 'Coming Soon!', `Coming Soon!`, `Coming Soon!`], i = 0;
     setInterval(() => client.user.setActivity(`*help | ${activities[i++ % activities.length]}`, { type: "WATCHING"}), 25000)
