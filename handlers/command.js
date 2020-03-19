a5629cae10254f57d2137be1277dd2fcd2b412ed
AlphaLupine <matthewduff0912@gmail.com>
2020-03-19 21:55:42 +0000

    [ADD] Added event and command handlers

---
+++ handlers/command.js
@@ -0,0 +1,19 @@
+const { readdirSync } = require("fs")
+
+
+module.exports = (client) => {
+    const load = dirs => {
+        const commands = readdirSync(`./commands/${dirs}/`).filter(d => d.endsWith('.js'))
+        console.log("-------------------------------")
+        console.log(`Loading Directory: "${dirs}"`)
+        console.log("-------------------------------")
+        for (let file of commands) {
+            console.log(`${file} loaded`)
+            const pull = require(`../commands/${dirs}/${file}`)
+            client.commands.set(pull.config.name, pull)
+            if(pull.config.aliases) pull.config.aliases.forEach(a => client.aliases.set(a, pull.config.name))
+
+        }
+    }  
+    [].forEach(x => load(x))
+}
