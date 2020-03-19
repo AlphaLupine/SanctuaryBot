c98893cefaa120d899168fa524a6cb91bbb6055a
AlphaLupine <matthewduff0912@gmail.com>
2020-03-19 22:18:59 +0000

    [FIX] Corrected preifx displayed in status message

--- events/client/ready.js
+++ events/client/ready.js
@@ -1,8 +1,10 @@
+const { prefix } = require("../../config.json")
+
 
 module.exports = async client => {
 
     console.log(`${client.user.tag} is now online!`)
     
     let activities = [ 'Coming Soon!', `Coming Soon!`, `Coming Soon!`], i = 0;
-    setInterval(() => client.user.setActivity(`*help | ${activities[i++ % activities.length]}`, { type: "WATCHING"}), 25000)
+    setInterval(() => client.user.setActivity(`${prefix}help | ${activities[i++ % activities.length]}`, { type: "WATCHING"}), 25000) //TODO Sync with DB
 }
